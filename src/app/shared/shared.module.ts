af1431e9a75ce992ded69a2e8ab4d32bb29113c9
sacomg <sacomg@gmail.com>
2017-05-20 10:58:14 +0200

    nuevo

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TwimpCardComponent } from './twimp/twimp-card/twimp-card.component';
+import { TwimpListComponent } from './twimplist/twimp-list/twimp-list.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [TwimpCardComponent],
-  exports: [TwimpCardComponent]
+  declarations: [TwimpCardComponent, TwimpListComponent],
+  exports: [TwimpCardComponent, TwimpListComponent]
 })
 export class SharedModule { }
